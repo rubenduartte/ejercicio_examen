bd830cbf614ce9824539f5e40459a8535a33514d
Ruben Duarte <ruben_duarte_m@hotmail.com>
2017-09-18 22:37:10 -0400

    Proyecto Terminado

--- src/java/py/com/ejercicio/bean/EmpleadoBean.java
+++ src/java/py/com/ejercicio/bean/EmpleadoBean.java
@@ -6,9 +6,17 @@
 package py.com.ejercicio.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import py.com.ejercicio.entidad.Empleado;
+import py.com.ejercicio.manager.EmpleadoFacade;
 
 /**
  *
@@ -16,12 +24,77 @@ import javax.enterprise.context.SessionScoped;
  */
 @Named(value = "empleadoBean")
 @SessionScoped
-public class EmpleadoBean implements Serializable{
+public class EmpleadoBean implements Serializable {
 
-    /**
-     * Creates a new instance of EmpleadoBean
-     */
+    private Empleado selected;
+    private List<Empleado> item = new ArrayList<>();
+
+    @EJB
+    private EmpleadoFacade empleadoEJB;
+
     public EmpleadoBean() {
     }
-    
+
+    @PostConstruct
+    public void init() {
+
+        selected = new Empleado();
+        item = empleadoEJB.findAll();
+
+    }
+
+    public String salvar() {
+
+        if (getSelected() != null) {
+
+            if (selected != null && selected.getId() == null) {
+
+                empleadoEJB.create(selected);
+
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro Agregado", "Agregado");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+
+                limpiar();
+                item = empleadoEJB.findAll();
+                return "empleadoList";
+            }
+
+            empleadoEJB.edit(selected);
+            limpiar();
+        }
+
+        return "empleadoList";
+
+    }
+
+    public void limpiar() {
+
+        selected = new Empleado();
+
+    }
+
+    public Empleado getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Empleado selected) {
+        this.selected = selected;
+    }
+
+    public List<Empleado> getItem() {
+        return item;
+    }
+
+    public void setItem(List<Empleado> item) {
+        this.item = item;
+    }
+
+    public EmpleadoFacade getEmpleadoEJB() {
+        return empleadoEJB;
+    }
+
+    public void setEmpleadoEJB(EmpleadoFacade empleadoEJB) {
+        this.empleadoEJB = empleadoEJB;
+    }
+
 }
